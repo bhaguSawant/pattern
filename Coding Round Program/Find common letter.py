ee426c776ecfef5481fd2e0c8963da2f0c78d94c
Bhagyashree Sawant <142791525+bhaguSawant@users.noreply.github.com>
2024-10-23 11:58:27 +0530

    Update Find common letter.py

--- Coding Round Program/Find common letter.py
+++ Coding Round Program/Find common letter.py
@@ -9,3 +9,15 @@ def common_letter():
     print(lst)
 
 common_letter()
+
+#------------------------------------------------------------------
+#Directly enter the number
+def common_number():
+    str1=(2,3,4,5,3)
+    str2=(3,6,4,3)
+    s1=set(str1)
+    s2=set(str2)
+    lst=s1&s2
+    print(lst)
+    
+common_number()
